78a186f096cef952a2a6b0f027f921528ee45841
SH_H264 <jslkap227@gmail.com>
2021-09-28 13:09:02 +0900

    유저 홈 프래그먼트 UI 추가

--- app/src/main/java/com/example/owere/fragment/signUpFragment/EmailInputFragment.kt
+++ app/src/main/java/com/example/owere/fragment/signUpFragment/EmailInputFragment.kt
@@ -1,13 +1,10 @@
 package com.example.owere.fragment.signUpFragment
 
 import android.os.Bundle
-import android.text.BoringLayout
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
-
 import com.example.owere.R
 import com.example.owere.databinding.EmailInputFragmentBinding
 
--- app/src/main/java/com/example/owere/fragment/userMainFragment/homeFragment/UserHomeFragment.kt
+++ app/src/main/java/com/example/owere/fragment/userMainFragment/homeFragment/UserHomeFragment.kt
@@ -1,50 +1,30 @@
 package com.example.owere.fragment.userMainFragment.homeFragment
 
-import android.content.Context
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
 import com.example.owere.R
 import com.example.owere.adapter.UserHomeTabAdapter
-import com.google.android.material.tabs.TabLayout
+import com.example.owere.databinding.UserHomeFragmentBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class UserHomeFragment: Fragment(R.layout.user_home_fragment) {
 
-    lateinit var tabLayout : TabLayout
-    lateinit var viewPager : ViewPager2
+    private var binding: UserHomeFragmentBinding? = null
     private val tabTitle = listOf("디자이너", "미용실")
-
-    /**
-     * tabLayout과 viewPager를 초기화하기 위해 사용했습니다.
-     */
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val rootView = super.onCreateView(inflater, container, savedInstanceState)
-        rootView?.let{
-            tabLayout = it.findViewById(R.id.userHomeTabLayout)
-            viewPager = it.findViewById(R.id.userHomeViewPager)
-        }
-
-        return rootView
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val userHomeFragmentBinding = UserHomeFragmentBinding.bind(view)
+        binding = userHomeFragmentBinding
 
         initTabLayout()
     }
 
     private fun initTabLayout(){
+        val tabLayout = binding?.userHomeTabLayout!!
+
         repeat(2) {
             tabLayout.addTab(tabLayout.newTab())    // 탭 2개 생성
         }
@@ -52,13 +32,18 @@ class UserHomeFragment: Fragment(R.layout.user_home_fragment) {
         initViewPager()
 
         // TabLayout과 ViewPager 연결 + 탭 타이틀 지정
-        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+        TabLayoutMediator(tabLayout, binding?.userHomeViewPager!!){ tab, position ->
             tab.text = tabTitle[position]
         }.attach()
     }
 
     private fun initViewPager(){
-        viewPager.isSaveEnabled = false
-        viewPager.adapter = UserHomeTabAdapter(this, tabLayout.tabCount)
+        binding?.userHomeViewPager?.apply{
+            isSaveEnabled = false
+            adapter = UserHomeTabAdapter(
+                this@UserHomeFragment,
+                binding?.userHomeTabLayout!!.tabCount
+            )
+        }
     }
 }

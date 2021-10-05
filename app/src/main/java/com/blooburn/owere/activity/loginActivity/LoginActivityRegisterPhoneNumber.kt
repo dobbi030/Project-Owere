9c3acbbf0f0e4bf5c8b5f29028c5177bba0d4064
river_is_line <dmstjs7047@hanmail.net>
2021-10-06 03:40:01 +0900

    계정 복구(이메일) UI

---
+++ app/src/main/java/com/blooburn/owere/activity/loginActivity/LoginActivityRegisterPhoneNumber.kt
@@ -0,0 +1,11 @@
+package com.blooburn.owere.activity.loginActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivityRegisterPhoneNumber : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register_phonenumber)
+    }
+}

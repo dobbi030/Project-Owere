18712fddc062d96c9c2e8ad4af63842d56d262f6
eigen98 <terrantrace@gmail.com>
2021-10-12 23:59:51 +0900

    사용자 예약내역 구현중.10/11

--- app/src/main/java/com/blooburn/owere/fragment/userMainFragment/reservationFragment/ReservationBottomDialogFrament.kt
+++ app/src/main/java/com/blooburn/owere/fragment/userMainFragment/reservationFragment/ReservationBottomDialogFrament.kt
@@ -36,7 +36,7 @@ class ReservationBottomDialogFrament : BottomSheetDialogFragment() {
 
         // 예약 취소 버튼
         var cancelButton = view.findViewById<TextView>(R.id.cancel_reservation)
-//
+//기본 alertDialog는 커스텀이 안 되어서 주석 처리 커스텀 다이얼로그 클래스 생성
 //        cancelButton.setOnClickListener {
 //            var builder = AlertDialog.Builder(requireContext())
 //                .setTitle("정말 예약을 취소하실건가요?")
@@ -55,7 +55,7 @@ class ReservationBottomDialogFrament : BottomSheetDialogFragment() {
 
 
 }
-//커스텀다이얼로그
+//커스텀다이얼로그 (예약을 취소하실 건가요?)
 class ReservationCustomDialog(context: Context) {
     private val dialog = Dialog(context)
 
@@ -80,18 +80,18 @@ class ReservationCustomDialog(context: Context) {
         yesButton.setOnClickListener {
 
             val dialog = ReservationYesDialog(context)
-            dialog.myDiag()
+            dialog.myDiag(context)
         }
 
         dialog.show()
     }
 }
 
-//커스텀다이얼로그
+//커스텀다이얼로그 (예약 취소 사유 선택)
 class ReservationYesDialog(context: Context) {
     private val dialog = Dialog(context)
 
-    fun myDiag() {
+    fun myDiag(context:Context) {
 //        보여질 화면 결정
         dialog.setContentView(R.layout.reservation_cancel_yes_layout)
 
@@ -104,6 +104,40 @@ class ReservationYesDialog(context: Context) {
         dialog.setCanceledOnTouchOutside(true)
         //취소 가능
         dialog.setCancelable(true)
+
+        val reasonForCancelFirst = dialog.findViewById<TextView>(R.id.why_cancel_first)
+        reasonForCancelFirst.setOnClickListener {  }
+
+        //직접입력 버튼
+        val inputItself= dialog.findViewById<TextView>(R.id.why_cancel_inputself)
+        inputItself.setOnClickListener {
+            val dialog = InputMyselfCustomDialog(context)
+            dialog.myDiag(context)
+            
+        }
+
+        dialog.show()
+    }
+}
+
+//커스텀다이얼로그(직접 입력 다이얼로그)
+class InputMyselfCustomDialog(context: Context) {
+    private val dialog = Dialog(context)
+
+    fun myDiag(context: Context) {
+//        보여질 화면 결정
+        dialog.setContentView(R.layout.reason_input_cancel_dialog_layout)
+
+        //Dialog 크기 설정
+        dialog.window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        //영역 밖 터치 처리
+        dialog.setCanceledOnTouchOutside(true)
+        //취소 가능
+        dialog.setCancelable(true)
+
 
 
 

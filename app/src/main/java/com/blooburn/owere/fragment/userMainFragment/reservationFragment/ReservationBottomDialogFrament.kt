36631ca660830995f1fa9533477962b247eae176
eigen98 <terrantrace@gmail.com>
2021-10-14 01:56:59 +0900

    마이페이지 구현중.10/14

--- app/src/main/java/com/blooburn/owere/fragment/userMainFragment/reservationFragment/ReservationBottomDialogFrament.kt
+++ app/src/main/java/com/blooburn/owere/fragment/userMainFragment/reservationFragment/ReservationBottomDialogFrament.kt
@@ -1,27 +1,31 @@
 package com.blooburn.owere.fragment.userMainFragment.reservationFragment
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivities
+import androidx.core.content.ContextCompat.startActivity
 import com.blooburn.owere.R
+import com.blooburn.owere.activity.userMain.userReservation.ReservationCanceled
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.util.*
 
 
 class ReservationBottomDialogFrament : BottomSheetDialogFragment() {
-
-    val positiveButtonClick = { dialogInterface: DialogInterface, i: Int ->
-        Toast.makeText(requireContext(), "positive", Toast.LENGTH_SHORT).show()
-    }
-    val negativeButtonClick = { dialogInterface: DialogInterface, i: Int ->
-        Toast.makeText(requireContext(), "negative", Toast.LENGTH_SHORT).show()
-    }
 
 
     override fun onCreateView(
@@ -36,6 +40,7 @@ class ReservationBottomDialogFrament : BottomSheetDialogFragment() {
 
         // 예약 취소 버튼
         var cancelButton = view.findViewById<TextView>(R.id.cancel_reservation)
+
 //기본 alertDialog는 커스텀이 안 되어서 주석 처리 커스텀 다이얼로그 클래스 생성
 //        cancelButton.setOnClickListener {
 //            var builder = AlertDialog.Builder(requireContext())
@@ -44,6 +49,7 @@ class ReservationBottomDialogFrament : BottomSheetDialogFragment() {
 //                .setNegativeButton("아니요", negativeButtonClick)
 //                .show()
 //        }
+
         cancelButton.setOnClickListener {
             val dialog = ReservationCustomDialog(requireContext())
             dialog.myDiag(requireContext())
@@ -138,8 +144,38 @@ class InputMyselfCustomDialog(context: Context) {
         //취소 가능
         dialog.setCancelable(true)
 
+        val buttonCancel = dialog.findViewById<TextView>(R.id.button_so_cancel)
 
+        val editTextReason = dialog.findViewById<EditText>(R.id.reason_edittext)
 
+        val textWatcher = object:TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                var length = s.toString().length
+                if(length!=0){
+                    buttonCancel.visibility = VISIBLE
+                }else{
+                    buttonCancel.visibility = GONE
+                }
+            }
+
+        }
+
+        editTextReason.addTextChangedListener(textWatcher)
+
+        //거절하기 버튼 리스너
+        buttonCancel.setOnClickListener {
+
+            val intent = Intent(context,ReservationCanceled::class.java)
+            startActivity(context,intent,null)
+        }
 
         dialog.show()
     }

9a6b65329adb3f38c7c719beeb4f60dd54cec821
river_is_line <dmstjs7047@hanmail.net>
2021-11-23 13:33:18 +0900

    Log out

--- app/src/main/java/com/blooburn/owere/user/activity/main/mypageActivity/SettingActivity.kt
+++ app/src/main/java/com/blooburn/owere/user/activity/main/mypageActivity/SettingActivity.kt
@@ -1,17 +1,30 @@
 package com.blooburn.owere.user.activity.main.mypageActivity
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.blooburn.owere.R
+import com.blooburn.owere.user.activity.main.mypageActivity.SettingActivity.Companion.firstActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-//환경설정
+
+
 class SettingActivity : AppCompatActivity() {
+    // MemberQuitDialog에서 SettingActivity를 종료시키기 위해 Activity를 전역변수로 선언
+    companion object {
+        private const val TAG = "MainActivityForUnKnownUser"
+        var firstActivity: Activity? = null
+    }
 
     val auth = FirebaseAuth.getInstance()
+
+    var googleSignInClient: GoogleSignInClient? = null
 
     private val logOutButton by lazy {
         findViewById<TextView>(R.id.setting_logout)
@@ -21,41 +34,39 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
-        if(auth.currentUser == null){
+        if (auth.currentUser == null) {
             this.finish()
         }
 
+        val gso: GoogleSignInOptions =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
 
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         logOutButton.setOnClickListener {
-            val dialog = MemeberQuitDialog(this,auth)
-            dialog.myDiag(this,auth)
-
-
-        }
-
-
-
-    }
-
-    //로그아웃 후 finish처리
-    override fun onResume() {
-        super.onResume()
-
-        if(auth.currentUser== null){
-            finish()
+            firstActivity = this
+            val dialog = MemeberQuitDialog(this, auth, googleSignInClient)
+            dialog.myDiag(this, auth)
         }
     }
-
-
 }
 
 //커스텀다이얼로그 (로그아웃 하실 건가요?)
-class MemeberQuitDialog(context: Context,auth : FirebaseAuth) {
+class MemeberQuitDialog(
+    context: Context,
+    auth: FirebaseAuth,
+    googleSignInClient: GoogleSignInClient?
+) {
     private val dialog = Dialog(context)
 
-    fun myDiag(context:Context,auth:FirebaseAuth) {
-//        보여질 화면 결정
+    private val mgoogleSignInClient: GoogleSignInClient? = googleSignInClient
+
+
+    fun myDiag(context: Context, auth: FirebaseAuth) {
+        // 보여질 화면 결정
         dialog.setContentView(R.layout.logout_dialog_layout)
 
         //Dialog 크기 설정
@@ -71,15 +82,15 @@ class MemeberQuitDialog(context: Context,auth : FirebaseAuth) {
         val yesButton = dialog.findViewById<TextView>(R.id.logout_yes)
         val noButton = dialog.findViewById<TextView>(R.id.logout_no)
 
-
         yesButton.setOnClickListener {
-
             auth.signOut()
-
+            mgoogleSignInClient?.signOut()
 
             //프래그먼트 삭제
             dialog.dismiss()
 
+            // 액티비티 종료 -> 로그인 화면으로 돌아감
+            firstActivity?.finish()
         }
 
         dialog.show()

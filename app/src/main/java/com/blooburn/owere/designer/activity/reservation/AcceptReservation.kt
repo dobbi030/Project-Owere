6645414a45bb8723b0338b79ba675bf3d796493b
eigen98 <terrantrace@gmail.com>
2021-11-29 13:00:40 +0900

    11/29 (2) 예약 수락 액티비티 인텐트 실수 수정

--- app/src/main/java/com/blooburn/owere/designer/activity/reservation/AcceptReservation.kt
+++ app/src/main/java/com/blooburn/owere/designer/activity/reservation/AcceptReservation.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.blooburn.owere.R
+import com.blooburn.owere.designer.activity.main.DesignerMainActivity
 import com.blooburn.owere.user.activity.main.UserMainActivity
 
 //디자이너 예약내역(수락대기중) -> 예약 수락 화면
@@ -27,7 +28,7 @@ class AcceptReservation : AppCompatActivity() {
 
 
         button.setOnClickListener {
-            val intent = Intent(this, UserMainActivity::class.java)
+            val intent = Intent(this, DesignerMainActivity::class.java)
             intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             //FLAG_ACTIVITY_CLEAR_TOP 사용하여 홈 액티비티가 호출 될 시 그 위에 있던 액티비티 모두 삭제
